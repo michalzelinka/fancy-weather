6c713c74bb4b4cff5d564fa6629698fadd9c02f2
Michal Zelinka <misacek@misacek.net>
2015-07-05 12:12:33 +0200

    Initial setup

---
+++ weather/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  weather
+//
+//  Created by Michi on 5/7/15.
+//  Copyright (c) 2015 Michi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+
+	func application(application: UIApplication,
+		didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+	{
+		return true
+	}
+
+	func applicationWillResignActive(application: UIApplication) { }
+
+	func applicationDidEnterBackground(application: UIApplication) { }
+
+	func applicationWillEnterForeground(application: UIApplication) { }
+
+	func applicationDidBecomeActive(application: UIApplication) { }
+
+	func applicationWillTerminate(application: UIApplication) { }
+
+}
